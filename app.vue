55ad1becdd4f133ff67de1b5e3e4a82f82332727
iqvverVIVO <iqvver@gmail.com>
2024-11-02 16:03:59 +0200

    add hero section

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 <template>
-  
+  <HeroSection />
 </template>
 <style scoped lang="scss"></style>
---
+++ components/HeroSection.vue
@@ -0,0 +1,101 @@
+<template>
+    <section class="hero-container">
+        <div class="l-container hero__content">
+            <div class="hero__logo-wrapper">
+                <div v-for="img in 4">
+                    <img src="../public//icons/horse.svg" class="hero__logo-img" alt="horse" />
+                </div>
+                <div class="hero__logo-text">Клуб <br />четырех коней</div>
+            </div>
+            <h1 class="hero__title">
+                Превратите уездный город
+                <div>в столицу земного шара</div>
+            </h1>
+            <div class="hero__subtitle">
+                Оплатите взнос на телеграммы для организации Международного васюкинского турнира по шахматам
+            </div>
+            <div class="hero__buttons">
+                <div class="hero__button l-primary-button">Поддержать шахматную мысль</div>
+                <div class="hero__button l-primary-button secondary">Подробнее о турнире</div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+    &-container {
+        height: 100vh;
+        background-image: url('../public/images/hero.png');
+        background-position: top;
+        background-repeat: no-repeat;
+        background-size: auto;
+
+        @media (width < 1366px) {
+            background-size: cover;
+        }
+    }
+
+    &__content {
+        padding: 26px 0 0 0;
+        margin: 0 auto;
+    }
+
+    &__logo {
+        &-wrapper {
+            width: 245px;
+            height: 36px;
+            display: flex;
+            align-items: center;
+            gap: 2px;
+
+            & img {
+                height: 100%;
+            }
+        }
+        &-text {
+            display: flex;
+            align-items: center;
+            width: 147px;
+            height: 100%;
+            color: #595653;
+            font-weight: 700;
+            font-size: 16.6513px;
+            line-height: 108%;
+        }
+    }
+
+    &__title {
+        width: 50%;
+        margin-top: 62px;
+        font-weight: 400;
+        font-size: 60px;
+        line-height: 110%;
+        text-transform: uppercase;
+        color: #313131;
+
+        & div {
+            text-align: center;
+        }
+    }
+
+    &__subtitle {
+        width: 50%;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 130%;
+        text-align: center;
+        color: #313131;
+    }
+
+    &__buttons {
+        display: flex;
+        gap: 19px;
+        margin-top: 40px;
+    }
+    &__button {
+        width: 263px;
+        height: 72px;
+    }
+}
+</style>
